733c68f455f04369843b23cf5e90c8e5a6a81eff
bryanagamk <bryanagamk@gmail.com>
2024-02-09 02:32:35 +0700

    feat: cors policy and getOne data

--- controllers/studentController/studentController.go
+++ controllers/studentController/studentController.go
@@ -5,16 +5,35 @@ import (
 
 	"github.com/bryanagamk/crud-go-api/models"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 func Index(c *gin.Context) {
 	var students []models.Student
 
 	models.DB.Find(&students)
-	c.JSON(http.StatusOK, gin.H{"students": students})
+	c.JSON(http.StatusOK, gin.H{"data": students})
 }
 
-func Store(c *gin.Context)  {}
-func Show(c *gin.Context)   {}
+func Store(c *gin.Context) {}
+
+func Show(c *gin.Context) {
+	var student models.Student
+	id := c.Param("id")
+
+	if err := models.DB.First(&student, id).Error; err != nil {
+		switch err {
+		case gorm.ErrRecordNotFound:
+			c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"message": "Data tidak ditemukan"})
+			return
+		default:
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": err})
+			return
+		}
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": student})
+}
+
 func Update(c *gin.Context) {}
 func Delete(c *gin.Context) {}
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	studentcontroller "github.com/bryanagamk/crud-go-api/controllers/studentController"
 	"github.com/bryanagamk/crud-go-api/models"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,6 +11,17 @@ func main() {
 
 	r := gin.Default()
 	models.ConnectDatabase()
+	corsConfig := cors.DefaultConfig()
+
+	corsConfig.AllowOrigins = []string{"http://localhost:5173"}
+	// To be able to send tokens to the server.
+	corsConfig.AllowCredentials = true
+
+	// OPTIONS method for ReactJS
+	corsConfig.AddAllowMethods("OPTIONS")
+
+	// Register the middleware
+	r.Use(cors.New(corsConfig))
 
 	r.GET("/students", studentcontroller.Index)
 	r.POST("/students", studentcontroller.Store)
